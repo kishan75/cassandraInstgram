0bff6586273b8078f1c840d633eb7f98531798f9
yogi10 <kurmiy@techracers.io>
2019-05-15 18:37:10 +0530

    make acitvityid and notfid same

--- controller/reply.js
+++ controller/reply.js
@@ -5,7 +5,6 @@ var collection = {};
 
 collection.addLike = function (req, res) {
     const notfid = TimeUuid.now();
-    const activityid = TimeUuid.now()
     const query1 = `UPDATE reply SET like = like + {'${req.body.fromuser}'} where replyid = ? `;
     const query2 = `INSERT INTO notification (notificationid , fromuser , touser , type , time) VALUES (?,?,?,?,?)`;
     const query3 = `UPDATE user SET notifications = notifications +{'${notfid}'} where userid = ?`;
@@ -25,7 +24,7 @@ collection.addLike = function (req, res) {
         },
         {
             query: query4,
-            params: [activityid,req.body.fromuser,req.body.touser,{message:'like-reply', id: req.body.replyid},new Date()]
+            params: [notfid,req.body.fromuser,req.body.touser,{message:'like-reply', id: req.body.replyid},new Date()]
         }
     ];
     var result = client.batch(queries, { prepare: true });
@@ -37,5 +36,36 @@ collection.addLike = function (req, res) {
         console.log(err);
     });
 };
-
+collection.deleteLike = function (req, res) {
+    const query1 = `UPDATE reply SET like = like - {'${req.body.fromuser}'} where replyid = ? `;
+    const query2 = `INSERT INTO notification (notificationid , fromuser , touser , type , time) VALUES (?,?,?,?,?)`;
+    const query3 = `UPDATE user SET notifications = notifications +{'${notfid}'} where userid = ?`;
+    const query4 = `INSERT INTO myactivity (activityid,byuser,ofuser,type,time) VALUES(?,?,?,?,?)`;
+    const queries = [
+        {
+            query: query1,
+            params: [req.body.replyid]
+        },
+        {
+            query: query2,
+            params: [notfid,req.body.userid,req.body.touser,{message:'like-reply', id: req.body.replyid},new Date()]
+        },
+        {
+            query:query3,
+            params: [req.body.touser]
+        },
+        {
+            query: query4,
+            params: [notfid,req.body.fromuser,req.body.touser,{message:'like-reply', id: req.body.replyid},new Date()]
+        }
+    ];
+    var result = client.batch(queries, { prepare: true });
+    result.then(result => {
+        console.log(result);
+        
+        res.send("reply liked");
+    }, err => {
+        console.log(err);
+    });
+};
 module.exports = collection;
