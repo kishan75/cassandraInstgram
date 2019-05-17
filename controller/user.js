7b96906dec447d457fb3bdfafebc7b24b6da36c7
kishan75 <kishan.chaurasiya.75@gmail.com>
2019-05-17 20:29:35 +0530

    changing user apis

--- controller/user.js
+++ controller/user.js
@@ -5,14 +5,12 @@ const TimeUuid = require('cassandra-driver').types.TimeUuid;
 var collection = {};
 
 collection.registerUser = function (req, res) {
-    req.body.registrationdate = dateFormat(new Date(), 'yyyy-mm-dd');
+    req.body.time = new Date();
     req.body.dob = dateFormat(req.body.dob, 'yyyy-mm-dd');
     req.body.userid = TimeUuid.now();
-    console.log(req.body.userid);
-    var query = "INSERT into user ( userid,dob,email,firstname,lastname,phone,registrationdate,username) VALUES( ?,?,?,?,?,?,?,?)";
+    var query = "INSERT into user ( userid,dob,email,firstname,lastname,phone,time,username) VALUES( ?,?,?,?,?,?,?,?)";
     var result = client.execute(query, req.body, { prepare: true });
-    result.then(result => console.log(result), err => console.log(err));
-    res.send(result);
+    result.then(result => res.send(result), err => console.log(err));
 };
 
 module.exports = collection;
